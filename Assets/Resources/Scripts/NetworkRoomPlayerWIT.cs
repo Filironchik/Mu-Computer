c4b4180dcb099529d0c2029475e1ca42adb97b2f
Filiron-Сова Айтишник <78251890+Filironchik@users.noreply.github.com>
2021-10-26 23:05:12 +0300

    Начало мультиплееру.
    Создал иерархию папок проекта. Создал префаб игрока, загрузил имающуюся графику (временно, потом надо будет полностью переделывать). Начал писать код подключения к лобби.

---
+++ Assets/Resources/Scripts/NetworkRoomPlayerWIT.cs
@@ -0,0 +1,116 @@
+﻿using Mirror;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+internal class NetworkRoomPlayer : NetworkBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private GameObject lobbuUI = null;
+    [SerializeField] private TMP_Text[] playerNameTexts = new TMP_Text[4];
+    [SerializeField] private TMP_Text[] playerReadeTexts = new TMP_Text[4];
+    [SerializeField] private Button startGameButton = null;
+
+    [SyncVar(hook = nameof(HandleDisplayNameChanged))]
+    public string DisplayName = "Loading...";
+    public bool IsReady = false;
+
+
+    private bool isLeader;
+    public bool IsLeLeader
+    {
+        set
+        {
+            isLeader = value;
+            startGameButton.gameObject.SetActive(value);
+        }
+    }
+
+    private NetworkManagerLobby room;
+    private NetworkManagerLobby Room
+    {
+        get
+        {
+            if (room != null) { return room; }
+            return room = NetworkManager.singleton as NetworkManagerLobby;
+        }
+    }
+
+    public override void OnStartAuthority()
+    {
+        CmdSetDisplayName(PlayerNameInput.DisplayName);
+        lobbuUI.SetActive(true);
+    }
+
+    public override void OnStartClient()
+    {
+        Room.RoomPlayers.Add(this);
+
+        UpdateDiaplay();
+    }
+
+    public override void OnStopClient()
+    {
+        Room.RoomPlayer.Remove(this);
+
+        UpdateDiaplay();
+    }
+
+    public void HandleReadyStatusChanged(bool oldValue, bool newValue) => UpdateDiaplay();
+    public void HandleDisplayNameChanged(string oldValue, string newValue) => UpdateDisplay();
+
+    private void UpdateDiaplay()
+    {
+        if (!hasAuthority)
+        {
+            foreach (var player in Room.RoomPLayers)
+            {
+                if (player.hasAuthority();
+                break;
+            }
+            return;
+        }
+        
+        for (int i = 0; i < playerNameTexts.Length; i++)
+        {
+            playerNameTexts[i].text = "Waiting For Player...";
+            playerReadeTexts[i].text = string.Empty;
+        }
+
+        for (int i = 0; i < Room.RoomPlayers.Count; i++)
+        {
+            playerNameTexts[i].text = Room.RoomPlayers[i].DisplayName;
+            playerReadeTexts[i].text = Room.RoomPlayers[i].IsReady ?
+                "<color=green>Ready</color>" :
+                "<color=red>Not Ready</color>";
+        }
+    }
+
+    public void HandleReadyToStart(bool readyToStart)
+    {
+        if (!isLeader) { return; }
+
+        startGameButton.interactable = readyToStart;
+    }
+
+    [Command]
+    private void CmdSetDisplayName(string displayName)
+    {
+        DisplayName = displayName;
+    }
+
+    [Command]
+    public void CmdReadyUp()
+    {
+        IsReady = !IsReady;
+        Room.NotifyPlayersOfReadyState();
+    }
+
+    [Command]
+    public void CmdStartGame()
+    {
+        if (Room.RoomPlayers[0].connectionClient != connectionToClient) { return; }
+        Room.StartGame();
+    }
+
+}
